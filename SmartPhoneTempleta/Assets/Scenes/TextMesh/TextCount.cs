9cb3eaccc5fb280ff9e0b30c42f855a72f92fef5
sekachan <seka.au@gmail.com>
2020-04-14 18:34:04 +0900

    Animationテスト修正 Navigationテスト修正 TextMeshProテスト追加

---
+++ SmartPhoneTempleta/Assets/Scenes/TextMesh/TextCount.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextCount : MonoBehaviour
+{
+    public int Count { get; set; } = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Count++;
+        this.GetComponent<TextMeshProUGUI>().SetText("<size=60%>value</size> {0}", Count);
+    }
+}
