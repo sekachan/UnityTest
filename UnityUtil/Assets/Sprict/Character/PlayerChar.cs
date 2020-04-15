6f1e4b41e885204f8836570a28cbc02ddcbc20c5
sekachan <seka.au@gmail.com>
2020-04-15 14:27:12 +0900

    よく使うAPIを試したサンプル

---
+++ UnityUtil/Assets/Sprict/Character/PlayerChar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// プレイヤーキャラクター
+/// </summary>
+public class PlayerChar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StatusData data = ScriptableObject.CreateInstance<StatusData>();
+
+        this.gameObject.transform.GetComponentInChildren<Text>().text = data.PlayerHP.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
