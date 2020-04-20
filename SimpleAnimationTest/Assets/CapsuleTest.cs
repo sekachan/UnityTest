95ccfff278c24545ec817255b6686f173ba8a207
sekachan <seka.au@gmail.com>
2020-04-20 11:57:44 +0900

    SimpleAnimationTest追加

---
+++ SimpleAnimationTest/Assets/CapsuleTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<SimpleAnimation>().Play("Default");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick()
+    {
+        this.GetComponent<SimpleAnimation>().Stop();
+        this.GetComponent<SimpleAnimation>().CrossFade("Anima1", 0.5f);
+    }
+}
